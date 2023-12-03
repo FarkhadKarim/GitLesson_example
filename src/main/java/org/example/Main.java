a4252ad80cd3735d7d5d9a4fcac35ff1897786b0
Farkhad Karim <kafa@Farkhads-MacBook-Air.local>
2023-12-03 21:04:27 +0600

    test commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        mainMethodMessage(); mainMethodInfo();
+    }
+    private static void mainMethodMessage() {
+        System.out.println("QA MESSAGE ");
+    }
+    private static void mainMethodInfo() {
+        System.out.println("QA INFO ");
+    }
+}
